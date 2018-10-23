8675e674e0c72c32335f7d66e4ceda9e0b8b5d7e
Christian Dominguez <christian@redbonzai.com>
2018-10-22 20:15:09 -0500

    FIlterThreadsByUser: Completed filtering threads by authenticated user.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,9 +33,18 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/threads">{{ __('All Threads') }}</a>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Select Threads By ...
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="nav-link" href="/threads">{{ __('All Threads') }}</a>
+                             @if( auth()->check())
+                                <a class="nav-link" href="/threads?by={{ auth()->user()->name }}">{{ __('My Threads') }}</a>
+                             @endif
+                            </div>
                         </li>
+
 
                         <li class="nav-item">
                             <a class="nav-link" href="/threads/create">{{ __('Create A Thread') }}</a>
